5b67ebbfd178b108aeb9592c23d3dae2a358c1ec
Aparna <aparnamolks.off@gmail.com>
2026-01-01 00:21:44 +0530

    Initial deployment setup

--- backend/server/src/index.js
+++ backend/server/src/index.js
@@ -2,15 +2,22 @@ const express = require('express')
 const cors = require('cors')
 
 
+
+
 const app = express()
 const rootRouter = require('./routes/index')
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "*", // or frontend Render URL later
+    credentials: true
+}));
+
+
 
 app.use('/api', rootRouter)
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
